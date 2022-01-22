1e59a6d0db7064529d69af4368540286c86d6ce0
José Roman <jroman@interbanking.com.ar>
2022-01-22 00:22:38 -0300

    Api funcionando

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from 'cors'
 import { routes } from "./routes.js";
 
 const PORT = 3000;
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
 
 routes(app);
 
